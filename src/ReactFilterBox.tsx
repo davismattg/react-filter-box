698ba9280b7349672e536ca9b62fd81905a99cfc
Matt Davis <matthewg_davis@apple.com>
2020-02-25 15:51:11 -0800

    allow search with errors

--- src/ReactFilterBox.tsx
+++ src/ReactFilterBox.tsx
@@ -46,9 +46,9 @@ export default class ReactFilterBox extends React.Component<any, any> {
 
     onSubmit(query: string) {
         var result = this.parser.parse(query);
-        if ((result as ParsedError).isError) {
-            return this.props.onParseError(result);
-        }
+        // if ((result as ParsedError).isError) {
+        //     return this.props.onParseError(result);
+        // }
 
         return this.props.onParseOk(result);
     }
